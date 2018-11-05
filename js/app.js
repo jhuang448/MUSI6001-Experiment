089cfb7c64b25043e5b106780a4c14a89b91bd43
Jiawen Huang <jhuang448@gatech.edu>
2018-11-05 03:49:11 -0500

    finished

---
+++ js/app.js
@@ -0,0 +1,168 @@
+//webkitURL is deprecated but nevertheless
+URL = window.URL || window.webkitURL;
+
+var gumStream; 						//stream from getUserMedia()
+var rec; 							//Recorder.js object
+var input; 							//MediaStreamAudioSourceNode we'll be recording
+
+// shim for AudioContext when it's not avb. 
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var audioContext //audio context to help us record
+
+var recordButton = document.getElementById("recordButton");
+var stopButton = document.getElementById("stopButton");
+var pauseButton = document.getElementById("pauseButton");
+
+//add events to those 2 buttons
+recordButton.addEventListener("click", startRecording);
+stopButton.addEventListener("click", stopRecording);
+pauseButton.addEventListener("click", pauseRecording);
+
+document.getElementById('record_status').style.display="none";
+
+function startRecording() {
+	console.log("recordButton clicked");
+	
+	document.getElementById('record_status').style.display="block";
+
+	/*
+		Simple constraints object, for more advanced audio features see
+		https://addpipe.com/blog/audio-constraints-getusermedia/
+	*/
+    
+    var constraints = { audio: true, video:false }
+
+ 	/*
+    	Disable the record button until we get a success or fail from getUserMedia() 
+	*/
+
+	recordButton.disabled = true;
+	stopButton.disabled = false;
+	pauseButton.disabled = false
+
+	/*
+    	We're using the standard promise based getUserMedia() 
+    	https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+	*/
+
+	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+		console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
+
+		/*
+			create an audio context after getUserMedia is called
+			sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
+			the sampleRate defaults to the one set in your OS for your playback device
+
+		*/
+		audioContext = new AudioContext();
+
+		//update the format 
+		document.getElementById("formats").innerHTML="Format: 1 channel pcm @ "+audioContext.sampleRate/1000+"kHz"
+
+		/*  assign to gumStream for later use  */
+		gumStream = stream;
+		
+		/* use the stream */
+		input = audioContext.createMediaStreamSource(stream);
+
+		/* 
+			Create the Recorder object and configure to record mono sound (1 channel)
+			Recording 2 channels  will double the file size
+		*/
+		rec = new Recorder(input,{numChannels:1})
+
+		//start the recording process
+		rec.record()
+
+		console.log("Recording started");
+
+	}).catch(function(err) {
+	  	//enable the record button if getUserMedia() fails
+    	recordButton.disabled = false;
+    	stopButton.disabled = true;
+    	pauseButton.disabled = true
+	});
+	
+	var audio = new Audio('audio/stimuli/metro.wav');
+	audio.play();
+}
+
+function pauseRecording(){
+	console.log("pauseButton clicked rec.recording=",rec.recording );
+	
+	document.getElementById('record_status').style.display="none";
+	
+	if (rec.recording){
+		//pause
+		rec.stop();
+		pauseButton.innerHTML="Resume";
+	}else{
+		//resume
+		rec.record()
+		pauseButton.innerHTML="Pause";
+
+	}
+}
+
+function stopRecording() {
+	console.log("stopButton clicked");
+
+	document.getElementById('record_status').style.display="none";
+	
+	//disable the stop button, enable the record too allow for new recordings
+	stopButton.disabled = true;
+	recordButton.disabled = true;
+	pauseButton.disabled = true;
+
+	//reset button just in case the recording is stopped while paused
+	pauseButton.innerHTML="Pause";
+	
+	//tell the recorder to stop the recording
+	rec.stop();
+
+	//stop microphone access
+	gumStream.getAudioTracks()[0].stop();
+
+	//create the wav blob and pass it on to createDownloadLink
+	rec.exportWAV(createDownloadLink);
+}
+function DeleteRecording(){
+	document.getElementById('recordingsList').innerHTML = "";
+}
+function createDownloadLink(blob) {
+	
+	var url = URL.createObjectURL(blob);
+	var au = document.createElement('audio');
+	var li = document.createElement('li');
+	var link = document.createElement('a');
+
+	//name of .wav file to use during upload and download (without extendion)
+	var filename = document.getElementById('studentID').innerHTML + '_' + document.getElementById('trialNO').innerHTML + '_' + document.getElementById('tNO').innerHTML;
+
+	//add controls to the <audio> element
+	au.controls = true;
+	au.src = url;
+
+	//save to disk link
+	link.href = url;
+	link.download = filename+".wav"; //download forces the browser to donwload the file using the  filename
+	link.innerHTML = "Do NOT Click";
+
+	document.getElementById('fileLink').href = url;
+	document.getElementById('fileLink').download = filename+".wav";
+	
+	//add the new audio element to li
+	li.appendChild(au);
+	
+	li.appendChild(document.createElement('br'));
+	
+	//add the filename to the li
+	li.appendChild(document.createTextNode(filename+".wav "))
+
+	//add the save to disk link to li
+	li.appendChild(link);
+	
+	saveButton.disabled = false;
+	
+	recordingsList.appendChild(li);
+}
---
+++ js/script.js
@@ -0,0 +1,97 @@
+document.getElementById('audio_status').style.display="none";
+
+var student_id = '970054364';
+var reaction_time;
+var audio_playing = false;
+var play_times = 0;
+var audio_tree = ['audio/stimuli/m2_sample.wav', 'audio/stimuli/m2_1.wav', 'audio/stimuli/m2_2.wav', 'audio/stimuli/m4_1.wav', 'audio/stimuli/m4_2.wav', 'audio/stimuli/m6_1.wav', 'audio/stimuli/m6_2.wav', 'audio/stimuli/m8_1.wav', 'audio/stimuli/m8_2.wav'];
+var random_file = [[0,1,3,5,7],[0,2,4,6,8],[0,1,3,5,7],[0,2,4,6,8]];
+var tnum = 0;
+var audio_idx = 0;
+
+function ROUND_TO(n, digits) {
+     if (digits === undefined) {
+       digits = 0;
+     }
+     var multiplicator = Math.pow(10, digits);
+     n = parseFloat((n * multiplicator).toFixed(11));
+     var test =(Math.round(n) / multiplicator);
+     return +(test.toFixed(digits));
+   }
+
+function hideD(){
+	document.getElementById('direction').hidden = true;
+}
+function showD(){
+	document.getElementById('direction').hidden = false;
+}
+   
+function enableNext(){
+	nextButton.disabled = false;
+	saveButton.disabled = true;
+}
+   
+function NextStimuli(){
+	audio_idx++;
+	if ( audio_idx == random_file[tnum].length ){
+		document.getElementById('message-title').innerHTML = "<h2>You have completed the experiment.Thank you!</h2>";
+	}
+	play_times = 0;
+	document.getElementById('message').innerHTML = 5-play_times;
+	document.getElementById('recordingsList').innerHTML = "";
+	recordButton.disabled = true;
+	stopButton.disabled = true;
+	pauseButton.disabled = true;
+	nextButton.disabled = true;
+	document.getElementById('audioIDX').innerHTML = "Playing NO." + (audio_idx) + " (" + (2*audio_idx) + " measures)";
+	document.getElementById('tNO').innerHTML = audio_idx;
+}
+ 
+function DeleteRecording(){
+	
+	document.getElementById('recordingsList').innerHTML = "";
+}
+ 
+function MAKE_A_SOUND(file)
+{
+	var audio = new Audio(file);
+	audio.play();
+	document.getElementById('audio_status').style.display="block";
+	audio.addEventListener("ended", function()
+	{
+		recordButton.disabled = false;
+	     audio.currentTime = 0;
+	     audio_playing = false;
+		 play_times++;
+		 
+		 document.getElementById('audio_status').style.display="none";
+		 document.getElementById('message').innerHTML = 5-play_times;
+	});
+}
+
+document.getElementById('send').addEventListener('click', () =>
+{
+	student_id = document.getElementById('name').value;
+	tnum = Number(document.getElementById('tnum').value)-1;
+	document.getElementById('enterID').innerHTML = "<h4>Your Student ID:</h4>";
+	document.getElementById('studentID').innerHTML = student_id;
+	document.getElementById('trialNO').innerHTML = tnum;
+	if (tnum == 0 || tnum == 2)
+		document.getElementById('tl').innerHTML = "<b><i>3. MOVE along to the rhythm while listening!</i></b>";
+	else if (tnum == 1 || tnum == 3)
+		document.getElementById('tl').innerHTML = "<b><i>3. DO NOT MOVE while listening.</i></b>";
+});
+
+document.addEventListener('keypress', function(LISTEN)
+{
+	if ( (LISTEN.keyCode == 112 || LISTEN.which == 112) && audio_playing === false )
+	{
+		audio_file = audio_tree[random_file[tnum][audio_idx]];
+		console.log('Playing: ' + audio_file);
+		if (play_times < 5){
+			MAKE_A_SOUND(audio_file);
+			audio_playing = true;
+		}
+	}
+	else {}
+});
